b8fc66b4d10db674d50226eb03b4977b5cbc8fa5
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-07-29 20:02:08 +0300

    img

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -363,7 +363,7 @@ const data = [
     {
         id:1,
         name:"JS-Algorithms",
-        image:"jsa",
+        image:"jsalgorithms",
         url:"https://github.com/MehmetCakir1/javascriptAlgorithms",
         source:"https://github.com/MehmetCakir1/javascriptAlgorithms",
         tool:"javascript"
