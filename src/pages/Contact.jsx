ea2892cb93023c55ae64d892b17f7276e36fe37b
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-07-29 20:27:40 +0300

    updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <article className="d-flex justify-content-center contact gap-2 flex-wrap">
+    <article className="d-flex justify-content-center contact gap-2 flex-column flex-sm-row flex-wrap align-items-center">
       <div className="text-light d-flex justify-content-center align-items-center contactLinkedin p-4 ">
         <Link
           to="//www.linkedin.com/in/mehmet%C3%A7ak%C4%B1r43/"
@@ -29,7 +29,7 @@ const Contact = () => {
       </div>
       <div className=" text-light  d-flex justify-content-center align-items-center contactEmail p-4  ">
         <a href="mailto:mehmetcakir4227@gmail.com" target="_blank">
-        <span className="display-2 d-flex align-items-center ">
+        <span className="display-2 d-flex align-items-center justify-content-center ">
           <BsFillEnvelopeFill/>
         </span>
         </a>
