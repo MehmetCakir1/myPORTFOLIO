49c8c24fb04c415d4e82840f1f963d499511987d
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-07-29 19:18:33 +0300

    js img

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,7 +10,7 @@ const Project = ({ name, image, url, source }) => {
       <div
         className="project-card col-sm-6 col-md-6 col-lg-4 d-flex justify-content-center align-items-center flex-column mb-2 mb-lg-4 rounded-3">
         <div className="project-imgDiv d-flex justify-content-center align-items-center" onMouseOver={()=>setShowBtn(false)} onMouseOut={()=>setShowBtn(true)}>
-          <img src={`images/${image}.PNG`} alt={name} className="rounded-3"/>
+          <img src={`/images/${image}.PNG`} alt={name} className="rounded-3"/>
           <div className={`btnDiv  flex-column justify-content-center align-items-center gap-1 rounded-3 ${showBtn ? "d-none" : "d-flex"}`}>
           <h3 className="text-white text-center projectName">{name}</h3>
         <Link to={`${url.slice(6)}`} target="_blank"><button className="btn visit border-0 fw-bold" >Visit Page</button></Link> 
