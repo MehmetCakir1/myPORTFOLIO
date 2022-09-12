1e6f115f618e43f92c7525582dd5caa688ce6ebb
MehmetCakir1 <mehmetcakir4227@gmail.com>
2022-09-13 01:59:58 +0300

    animatiom added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="home d-flex justify-content-center align-items-center">
+    <div className="home d-flex justify-content-center align-items-center flex-column">
       <div className="container m-auto col-9 homeContainer">
         {/* <h3 className="text-danger mt-5 h-6">Hi My name is</h3> */}
         <h2 className="text-light fullName">Mehmet Çakır</h2>
@@ -10,8 +10,20 @@ const Home = () => {
           experiences. Currently, I’m focused on building responsive
           web applications.
         </h5>
+        <div className="d-flex justify-content-between">
+             <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
       </div>
-    </div>
+        </div>
+      </div>
+
   );
 };
 
